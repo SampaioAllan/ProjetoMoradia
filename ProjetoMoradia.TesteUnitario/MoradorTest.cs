aaa48927f398bf91a8e69fa6c8cac0f2d0c73496
SampaioAllan <lanbrendo93@gmail.com>
2022-04-28 23:01:45 -0300

    criando teste para os metodos get e set do nome do morador, também garanti que o metodo seria usado dentro do construtor

--- ProjetoMoradia.TesteUnitario/MoradorTest.cs
+++ ProjetoMoradia.TesteUnitario/MoradorTest.cs
@@ -17,19 +17,23 @@ public class MoradorTest
 
         Assert.Equal(dataNascimentoEsperada, dataNascimento.ToString("dd/MM/yyyy"));
     }
-     [Fact]
+    [Fact]
     public void MoradorNaoDeveTerDataNascimentoMaiorQueADataAtual()
     {
         //Preparando - Arrange
-        var dataNascimento = "29/04/2022";
+        var dataNascimento = "29/04/2021";
         var morador = new Morador("Giovanni", "44455566678", dataNascimento);
-        //DataNascimento - Act
-        var dataNascimentosalva = morador.GetDataDeNascimento().ToString("dd/MM/yyyy");
-        
-        
-        //"datanascimento" é a data que foi colocada na primeira linha(ela precisa ser convertida para "DateTime")
-        //se a função la na classe "Morado.cs" funcionar, "dataNascimentoAtual" vai tero mesmo valor que a "dataNascimento"
-        
-        Assert.Equal(dataNascimento, dataNascimentosalva);
+
+        //DataNascimento - Act        
+        Assert.Equal(dataNascimento, morador.GetDataDeNascimento().ToString("dd/MM/yyyy"));
+    }
+
+    [Fact]
+    public void SalvarNomeMorador()
+    {
+        var nome = "victor";
+        var morador = new Morador(nome, "12345678910", "29/04/2022");
+
+        Assert.Equal(nome, morador.GetNome());
     }
 }
